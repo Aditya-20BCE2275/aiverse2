4fe80109dbe900b9e86e1b25c414d46e1c03740e
Aditya-20BCE2275 <amohite7100@gmail.com>
2023-07-27 17:41:58 +0530

    25min

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+const DashBoard = () => {
+  return (
+    <p>DashBoard Page (Protected)</p>
+  );
+};
+
+export default DashBoard;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+import { Deferred } from "next/dist/server/image-optimizer";
+
+const LandingPage = () => {
+  return (<div>Landing Page (Unprotected)</div>);
+};
+
+export default LandingPage;
