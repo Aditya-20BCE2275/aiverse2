7489cabae41d8eedb4c85a7535e822ce99b0190c
Aditya-20BCE2275 <amohite7100@gmail.com>
2023-07-29 00:19:43 +0530

    39.47
    Clerk Authentication completed

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,12 @@
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const DashBoard = () => {
   return (
+    <div>
     <p>DashBoard Page (Protected)</p>
+    <UserButton afterSignOutUrl="/"/>
+    </div>
   );
 };
 
