1c15ef1d4838438f761e53befcd6fc64cbb98e20
Aditya-20BCE2275 <amohite7100@gmail.com>
2023-08-18 11:59:06 +0530

    singup

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -25,7 +25,7 @@ export const LandingNavbar = () => {
         </h1>
       </Link>
       <div className="flex tems-center gap-x-2">
-        <Link href={isSignedIn ? "/dashboard" : "/singup"}>
+        <Link href={isSignedIn ? "/dashboard" : "/signup"}>
             <Button variant="outline" className="rounded-full">
                 Get Started
             </Button>
